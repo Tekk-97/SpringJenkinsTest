a1ea2b724a32f7e9ef511356e6c0c9651fb8aa77
Tekk-97 <jo22033@naver.com>
2024-04-08 14:43:28 +0900

    Spring boot first

---
+++ src/main/java/com/example/SpringJenkinsTest/SpringJenkinsTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringJenkinsTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringJenkinsTest/SpringJenkinsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringJenkinsTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
